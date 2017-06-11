3b3d23d3715a653ce6d0c37d059e142a03d81bf5
Matthieu <matthieunadeau96@gmail.com>
2017-06-11 14:57:15 +0000

    Checkpoint 2: Initialize and authorize(Fixed)

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,20 +1,20 @@
 require 'httparty'
 
 class Kele
-    require HTTParty
+    include HTTParty
     
     
     def initialize(email, password)
-        response = self.class.post(base_api_endpoint("sessions"), body: { "email": email, "password": password })
-        puts response.code
-        raise "Invalid email or password" if response.code == 404
-        @auth_token = response["auth_token"]
+        # response = self.class.post(base_api_endpoint("sessions"), body: { "email": email, "password": password })
+        # puts response.code
+        # raise "Invalid email or password" if response.code == 404
+        # @auth_token = response["auth_token"]
     end
     
-    private 
+    # private 
     
-    def base_api_endpoint(end_point)
-        "https://www.bloc.io/api/v1/#{end_point}"
-    end
+    # def base_api_endpoint(end_point)
+    #     "https://www.bloc.io/api/v1/#{end_point}"
+    # end
     
 end
