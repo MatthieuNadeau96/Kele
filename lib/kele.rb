bd94b7e18dc645661d7b9fc38d77d2e5f6aa4357
Matthieu <matthieunadeau96@gmail.com>
2017-06-12 17:01:43 +0000

    5: Roadmaps and Checkpoints

--- lib/kele.rb
+++ lib/kele.rb
@@ -27,6 +27,16 @@ class Kele
         @mentor_availability = JSON.parse(response.body)
     end
     
+    def get_roadmap(roadmap_id)
+        response = self.class.get(api_url("roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token })
+        @roadmap = JSON.parse(response.body)
+    end
+    
+    def get_checkpoints(checkpoint_id)
+        response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
+        @checkpoint = JSON.parse(response.body)
+    end
+    
     private
     
     def api_url(endpoint)
